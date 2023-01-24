83db58dcbedf0c759f4eabdab60055edf9558839
mukabyl <mukabyl@yamdex.ru>
2023-01-24 21:39:36 +0300

    first commit

--- src/Main.java
+++ src/Main.java
@@ -9,16 +9,16 @@ public class Main {
     public static void main(String[] args) {
 
         Bus bus = new Bus("2018.09.25", "blue", 12048);
-        bus.setSeller(new Seller("1982.06.19 ", " girl ", 555478962));
-        bus.setBusdriver(new Busdriver(" 1985.12.03 ", "Boy ", 222126548));
+        bus.setSeller(new Seller("Aigul ", " girl ", 555478962));
+        bus.setBusdriver(new Busdriver(" Taalai ", "Boy ", 222126548));
 
         Car car = new Car("2020.02.15", "yellow", 65014);
-        car.setTaxidriver(new Taxidriver(" 1992.02.26 ", " boy ", 778548756));
-        car.setBusdriver(new Busdriver(" 1985.12.03 ", "Boy ", 222126548));
+        car.setTaxidriver(new Taxidriver(" Asan ", " boy ", 778548756));
+        car.setBusdriver(new Busdriver(" Taalai ", "Boy ", 222126548));
 
         Trolleybus trolleybus = new Trolleybus("2020.06.04", "blue white", 80457);
-        trolleybus.setSeller(new Seller("1982.06.19 ", " girl ", 555478962));
-        trolleybus.setBusdriver(new Busdriver(" 1985.12.03 ", "Boy ", 222126548));
+        trolleybus.setSeller(new Seller("Aigul ", " girl ", 555478962));
+        trolleybus.setBusdriver(new Busdriver(" Taalai ", "Boy ", 222126548));
 
 
         System.out.println(bus);
